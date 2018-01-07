3f178e6a7c3c16145b6b0c5300a4b1c0418a53fd
luixal <luixal@gmail.com>
2018-01-07 20:39:50 +0100

    Updates version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'luixal:blaze-paginated-custom-list',
-  version: '0.0.7',
+  version: '0.0.8',
   // Brief, one-line summary of the package.
   summary: 'Paginated Custom List for Meteor and Blaze',
   // URL to the Git repository containing the source code for this package.
